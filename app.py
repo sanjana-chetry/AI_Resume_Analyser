068c40887d354efb446f4b46df87ad101eddf4fa
koushiiik <koushikmahanta52@gmail.com>
2026-01-27 20:18:43 +0530

    check if the file is uploaded or not

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import os
 
 app = Flask(__name__)
 
@@ -6,5 +7,18 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    file = request.files["resume"]         # Receive file from form
+
+    if file.filename == "":
+        return "No file selected"
+
+    # Save file in uploads folder
+    file_path = os.path.join("uploads", file.filename)
+    file.save(file_path)
+
+    return "File uploaded successfully!"   # Temporary response
 if __name__ == "__main__":
     app.run(debug=True)
