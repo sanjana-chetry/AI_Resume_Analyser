6ffabe92c1ac360c87dbea4075755c4a2da7ca99
sanjana-chetry <sanjanachetry118@gmail.com>
2026-02-16 23:33:35 +0530

    added role selection dropdown after upload and fixed layout

--- static/script.js
+++ static/script.js
@@ -2,14 +2,22 @@ const file_inp=document.getElementById("resume-input");
 const subtitle=document.querySelector(".upload-subtitle")
 const uploadBox = document.querySelector(".upload-box");
 
-file_inp.addEventListener("change",function(){
+const roleSection = document.getElementById("roleSection");
+const roleSelect = document.getElementById("roleSelect");
+const analyseBtn = document.getElementById("analyseBtn");
+
+file_inp.addEventListener("change",function(){  //UPLOAD LOGIC
     if(file_inp.files.length > 0){
         subtitle.textContent= file_inp.files[0].name + " selected";
         uploadBox.classList.add("active");
-        uploadBox.classList.add("loading");
-        file_inp.form.submit();
+        
+        //Show dropDown Section
+        roleSection.classList.add("show");
+        roleSelect.disabled = false;
     }
 });
+
+//DRAG AND DROP
 uploadBox.addEventListener("dragover",function(e){
     e.preventDefault();
     uploadBox.classList.add("dragover");
@@ -25,7 +33,27 @@ uploadBox.addEventListener("drop",function(e){
         file_inp.files=e.dataTransfer.files;
         subtitle.textContent=file_inp.files[0].name + " selected";
         uploadBox.classList.add("active");
-        uploadBox.classList.add("loading");
-        file_inp.form.submit();
+        
+        roleSection.classList.add("show");
+        roleSelect.disabled = false;
     }
+});
+
+//ROLE SELECTION
+roleSection.addEventListener("change",function(){
+    if(roleSection.value !==""){
+        analyseBtn.disabled=false;
+        analyseBtn.classList.add("enabled");
+    }
+    else{
+        analyseBtn.disabled=true;
+        analyseBtn.classList.remove("enabled");
+    }
+});
+
+analyseBtn.addEventListener("click",function(){
+    if(analyseBtn.disabled) return;
+
+    analyseBtn.disabled=true;
+    analyseBtn.innerHTML="Analysing"
 });
